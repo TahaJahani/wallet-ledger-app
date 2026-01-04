83f1ca0cbb3160503b104de2781519cf921e6962
TahaJahani <taha7900@gmail.com>
2026-01-04 21:11:43 +0330

    Add balance to wallet

--- core/models/wallet.py
+++ core/models/wallet.py
@@ -1,7 +1,36 @@
 import uuid
 
 from django.db import models
+from django.db.models.aggregates import Sum
+from django.db.models.expressions import Case, When, F
+from django.db.models.fields import IntegerField
 
 
 class Wallet(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    @property
+    def balance(self):
+        from .transaction import Transaction
+        result = self.transactions.aggregate(
+            balance=Sum(
+                Case(
+                    When(
+                        type__in=[
+                            Transaction.Type.deposit,
+                            Transaction.Type.transfer_in,
+                        ],
+                        then=F("amount"),
+                    ),
+                    When(
+                        type__in=[
+                            Transaction.Type.withdrawal,
+                            Transaction.Type.transfer_out,
+                        ],
+                        then=-F("amount"),
+                    ),
+                    output_field=IntegerField(),
+                )
+            )
+        )
+        return result["balance"] or 0
